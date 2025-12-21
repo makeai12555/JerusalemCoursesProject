4de8f1f8eb059d237de1429e5660a9c65a67f5f1
makeai12555 <makeai12555@gmail.com>
2025-12-21 08:14:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
-//fetch("/api/course-data")
- // .then(response => response.json())
- // .then(data => {
 
-    // טקסטים
-    document.getElementById("headline").innerText = data.headline;
-    document.getElementById("subheadline").innerText = data.subheadline;
-    document.getElementById("description").innerText = data.description;
-    document.getElementById("cta").innerText = data.cta;
 
-    // צבעים
-    document.body.style.backgroundColor = data.colors.primary;
-    document.getElementById("cta").style.backgroundColor = data.colors.secondary;
-  })
-  .catch(err => {
-    console.error("Failed to load course data", err);
-  });
